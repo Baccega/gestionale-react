791f862a34502410a0b6f1e58a3f265ffeb6a980
Sandro Baccega <sandrobac@hotmail.it>
2019-10-21 15:59:22 +0200

    Added frontend to docker-compose :whale:

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const hostname = "0.0.0.0";
 const port = 8080;
+const allowed_origins = ["http://0.0.0.0:3000"];
 
 const fastify = require("fastify")({ logger: true });
 
 const start = async () => {
 	try {
+		fastify.register(require("fastify-cors"), {
+			origin: allowed_origins
+		});
+
 		fastify.register(require("./routes"), { prefix: "/v1" });
 
 		await fastify.listen(port, hostname);
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -4,10 +4,19 @@ import { Context } from "../App";
 
 const styles = theme => ({});
 
+const testAPI = async () => {
+	const res = await fetch("http://0.0.0.0:3500/v1");
+	console.log(res);
+	const json = await res.json();
+	console.log(json);
+};
+
 function Dashboard(props) {
 	// const { classes } = props;
 
 	const { appState } = useContext(Context);
+
+	testAPI();
 
 	return <>{appState.udm[0]}</>;
 }
